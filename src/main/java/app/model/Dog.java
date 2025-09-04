67770f79289bbea652972bbb2af957e79b52c5fb
RedGho7T <redgho7t@gmail.com>
2025-09-04 09:02:42 +0300

    Spring Beans
    Remove @Autowired in class Dog
    CodeStyle

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -1,13 +1,11 @@
 package app.model;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Dog extends Animal {
 
-@Autowired
 @Qualifier("dog")
 private Animal animal;
 
